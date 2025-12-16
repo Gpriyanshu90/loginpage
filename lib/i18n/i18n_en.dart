8046ebe2fed0ed626204c456d97d17f53cfdc379
Gpriyanshu90 <gpriyanshu848@gmail.com>
2025-12-16 12:07:38 +0530

    final

---
+++ lib/i18n/i18n_en.dart
@@ -0,0 +1,6 @@
+const Map<String, String> en = {
+  "login": "Login",
+  "email": "Email",
+  "password": "Password",
+  "forgot": "Forgot Password?",
+};
---
+++ lib/i18n/i18n_hi.dart
@@ -0,0 +1,6 @@
+const Map<String, String> hi = {
+  "login": "लॉगिन",
+  "email": "ईमेल",
+  "password": "पासवर्ड",
+  "forgot": "पासवर्ड भूल गए?",
+};
